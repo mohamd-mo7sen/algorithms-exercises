bc9b679da1ec2b6e59f3f71199fcc75a1f55a7ce
Mohamed Mohsen <153040870+mohamd-mo7sen@users.noreply.github.com>
2024-01-08 17:10:57 +0200

    Update insertion-sort.test.js

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -15,11 +15,28 @@
 
 function insertionSort(nums) {
   // code goes here
+    for (let i = 1; i < nums.length; i++) {
+        let key = nums[i];
+        let j = i - 1;
+
+        // Move elements of arr[0..i-1] that are greater than key
+        // to one position ahead of their current position
+        while (j >= 0 && nums[j] > key) {
+            arr[j + 1] = nums[j];
+            j = j - 1;
+        }
+        nums[j + 1] = key;
+    }
+    return nums;
 }
+
+// Usage
+let arr = [12, 11, 13, 5, 6];
+console.log(insertionSort(arr));  // Output: [5, 6, 11, 12, 13]
 
 // unit tests
 // do not modify the below code
-test.skip("insertion sort", function () {
+test("insertion sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   insertionSort(nums);
   expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
