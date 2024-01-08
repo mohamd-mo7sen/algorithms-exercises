42b3a954b65e02cb0ec8969cfcd107f6ad29ecb1
Mohamed Mohsen <153040870+mohamd-mo7sen@users.noreply.github.com>
2024-01-08 16:15:56 +0200

    Update bubble-sort.test.js

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -12,6 +12,19 @@
 
 function bubbleSort(nums) {
   // code goes here
+  
+    let len = nums.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - i - 1; j++) {
+            if (nums[j] > nums[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                let temp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
 }
 
 // unit tests
